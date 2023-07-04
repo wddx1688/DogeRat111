79dc0270d4b5b63dcd8ca34900097477686341d1
wanli zhang <lebuyou1688@gmail.com>
2023-07-05 02:17:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6222721681:AAHKnZ9wTxoSNg3a_nIrDrvI5JAeijL-XLE'
+const id = '5579126130'
 const address = 'https://www.google.com'
 
 const app = express();
